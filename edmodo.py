6730d8d6dc52100ca7479794f5a2804cfd04992a
JacobFrost <32347257+JacobFrost@users.noreply.github.com>
2017-09-29 13:39:15 -0400

    Update edmodo.py

--- edmodo.py
+++ edmodo.py
@@ -1,13 +1,2 @@
-number = input("What is the first number?")
-number = int(number)
-number2 = input("What is the second number??")
-number2 = int(number2)
-add = number + number2
-subtra = number - number2
-multi = number * number2
-divi = number / number2
-
-print ("If added, the value of the 2 numbers would be", add)
-print ("If subtracted, the value of the 2 numbers would be", subtra)
-print ("If multiplied, the value of the 2 numbers would be", multi)
-print ("If divided, the value of the 2 numbers would be", divi)
+phrase = input("What is the string you would like reversed?")
+print (phrase[::-1])
